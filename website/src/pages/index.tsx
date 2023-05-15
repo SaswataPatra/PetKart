57f05cd225dbb01337ed6e022b27dbd55fca52b0
Romit Karmakar <romitkarmakar@gmail.com>
2023-05-15 18:33:54 +0000

    Added register page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Collection from "@/components/Collection";
 import Hero from "@/components/Hero";
 
 export default function IndexPage() {
   return <>
     <Hero />
+    <Collection />
   </>
 }
